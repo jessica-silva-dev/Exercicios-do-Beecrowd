8f010e90fba70c6cad14f56358bb1c53867a4880
Jéssica Silva <jessica.akyla@gmail.com>
2024-05-28 17:00:34 -0300

    Exercício que calcula o produto.

---
+++ EXERCICIOS/src/ProdutoSimples.java
@@ -0,0 +1,20 @@
+/*
+ Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.   
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+ */
+import java.util.Scanner;
+
+public class ProdutoSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int prod = x * y;
+        System.out.println("PROD = " + prod);
+    }
+}
